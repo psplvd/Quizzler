cd08253bb794dc24d6a0f170d753948df60fed0a
Dmitry <psplvd@gmail.com>
2023-01-30 14:29:32 +0100

    Расширен функционал.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Quizzler-iOS13
-//
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -15,15 +7,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    // questionLabel.text = "4 + 2 = 6"
+    let quiz = [
+    ["4 + 2 = 6", "True"],
+    ["5 - 3 > 1", "True"],
+    ["3 + 8 < 10", "False"]
+    ]
+    
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateUI()
     }
     @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Правильно!")
+        } else { print("Ошибка!") }
+        
+        
+        if questionNumber+1 < quiz.count {
+            questionNumber += 1
+        
+        } else { questionNumber = 0 }
+        updateUI()
     }
-    
-
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
+    }
 }
-
