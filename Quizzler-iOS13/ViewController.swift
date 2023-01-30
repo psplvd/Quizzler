1c41bb3a7365b70a456d31c314b44ff62ad489c3
Dmitry <psplvd@gmail.com>
2023-01-30 13:09:59 +0100

    Add iboutlets and ibaction

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    // questionLabel.text = "4 + 2 = 6"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+    }
+    
 
 }
 
